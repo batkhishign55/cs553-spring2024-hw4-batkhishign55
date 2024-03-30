ab031795a8294f4bf7540025be039007129129ac
bk-torus <batkhishign55@gmail.com>
2024-03-30 05:00:22 +0000

    graphs added

--- c/plot.py
+++ c/plot.py
@@ -4,108 +4,96 @@ import matplotlib.pyplot as plt
 import os
 
 res = []
+hash_plot=[[0 for _ in range(3)] for _ in range(3)]
+sort_plot=[[0 for _ in range(3)] for _ in range(3)]
+write_plot=[[0 for _ in range(3)] for _ in range(3)]
+            
+x = [1, 4, 16]
 
 def extract_info():
     file = open('./result.log', 'r')
     lines = file.readlines()
+    entry = {}
 
     for line in lines:
-        entry = {}
         if line.startswith('NUM_THREADS_HASH'):
-            print(line[18:])
+            # print("hash "+line[18:])
             entry['hash'] = int(line[18:])
         if line.startswith('NUM_THREADS_SORT'):
-            print(line[18:])
+            # print("sort "+line[18:])
             entry['sort'] = int(line[18:])
         if line.startswith('NUM_THREADS_WRITE'):
-            print(line[19:])
-            entry['write']= int(line[10:])
+            # print("write "+line[19:])
+            entry['write']= int(line[19:])
         if line.startswith('Elapsed time (s)'):
-            print(line[18:])
-            entry['time']=int (line[18:])
-            res.append(entry)
+            print("{:.2f}%".format(100-float(line[18:])/111.598420*100))
+            entry['time']= float(line[18:])
+            # print(entry)
+            res.append(entry['time'])
+            if entry['hash']==entry['sort']:
+                write_plot[x.index(entry['hash'])][x.index(entry['write'])]=entry['time']
+            if entry['write']==entry['sort']:
+                hash_plot[x.index(entry['write'])][x.index(entry['hash'])]=entry['time']
+            if entry['hash']==entry['write']:
+                sort_plot[x.index(entry['hash'])][x.index(entry['sort'])]=entry['time']
             entry = {}
-            
-x = [1, 2, 4, 8, 16, 32, 64]
 
+xs = ["1", "4", "16"]
 
-def plot_cpu_data():
-    fig, axs = plt.subplots(2)
-    axs[0].plot(x, cpu_data['eps'][0], label="baremetal")
-    axs[0].plot(x, cpu_data['eps'][1], label="container")
-    axs[0].plot(x, cpu_data['eps'][2], label="vm")
-    axs[0].set_title('CPU - Events per Second')
-    axs[0].legend()
+def plot_data():
 
-    axs[1].plot(x, cpu_data['lat'][0], label="baremetal")
-    axs[1].plot(x, cpu_data['lat'][1], label="container")
-    axs[1].plot(x, cpu_data['lat'][2], label="vm")
-    axs[1].set_title('CPU - Average Latency (ms)')
-    axs[1].legend()
+    fig, axs = plt.subplots(1)
+    axs.plot(xs, hash_plot[0], label="sort, write 1 thread")
+    axs.plot(xs, hash_plot[1], label="sort, write 4 thread")
+    axs.plot(xs, hash_plot[2], label="sort, write 16 thread")
+    axs.set_title('Hash - thread performance')
+    axs.legend()
+    axs.set_ylabel('Elapsed time(s)')
 
     fig.tight_layout()
-    plt.savefig('cpu.png')
+    plt.savefig('hash.png')
 
-
-def plot_mem_data():
-
-    fig, axs = plt.subplots(2)
-    axs[0].plot(mem_data['to'][0], label="baremetal")
-    axs[0].plot(mem_data['to'][1], label="container")
-    axs[0].plot(mem_data['to'][2], label="vm")
-    axs[0].set_title('Memory - Total Operations')
-    axs[0].legend()
-
-    axs[1].plot(mem_data['tp'][0], label="baremetal")
-    axs[1].plot(mem_data['tp'][1], label="container")
-    axs[1].plot(mem_data['tp'][2], label="vm")
-    axs[1].set_title('Memory - Throughput (MiB/sec)')
-    axs[1].legend()
+    fig, axs = plt.subplots(1)
+    axs.plot(xs, sort_plot[0], label="hash, write 1 thread")
+    axs.plot(xs, sort_plot[1], label="hash, write 4 thread")
+    axs.plot(xs, sort_plot[2], label="hash, write 16 thread")
+    axs.set_title('Sort - thread performance')
+    axs.legend()
+    axs.set_ylabel('Elapsed time(s)')
 
     fig.tight_layout()
-    plt.savefig('mem.png')
+    plt.savefig('sort.png')
 
-
-def plot_disk_data():
-
-    fig, axs = plt.subplots(2)
-    axs[0].plot(disk_data['to'][0], label="baremetal")
-    axs[0].plot(disk_data['to'][1], label="container")
-    axs[0].plot(disk_data['to'][2], label="vm")
-    axs[0].set_title('Disk - Total Operations')
-    axs[0].legend()
-
-    axs[1].plot(disk_data['tp'][0], label="baremetal")
-    axs[1].plot(disk_data['tp'][1], label="container")
-    axs[1].plot(disk_data['tp'][2], label="vm")
-    axs[1].set_title('Disk - Throughput (MiB/sec)')
-    axs[1].legend()
+    fig, axs = plt.subplots(1)
+    axs.plot(xs, write_plot[0], label="sort, hash 1 thread")
+    axs.plot(xs, write_plot[1], label="sort, hash 4 thread")
+    axs.plot(xs, write_plot[2], label="sort, hash 16 thread")
+    axs.set_title('Write - thread performance')
+    axs.legend()
+    axs.set_ylabel('Elapsed time(s)')
 
     fig.tight_layout()
-    plt.savefig('disk.png')
+    plt.savefig('write.png')
 
+    xss=[]
+    for i in (1,4,16):
+        for j in (1,4,16):
+            for k in (1,4,16):
+                xss.append("h"+str(i)+" s"+str(j)+" w"+str(k))
 
-def plot_net_data():
-
-    fig, axs = plt.subplots(2)
-    axs[0].plot(net_data['lat'][0], label="baremetal")
-    axs[0].plot(net_data['lat'][1], label="container")
-    axs[0].plot(net_data['lat'][2], label="vm")
-    axs[0].set_title('Network - Latency')
-    axs[0].legend()
-
-    axs[1].plot(net_data['tp'][0], label="baremetal")
-    axs[1].plot(net_data['tp'][1], label="container")
-    axs[1].plot(net_data['tp'][2], label="vm")
-    axs[1].set_title('Network - Throughput (Gbits/sec)')
-    axs[1].legend()
+    fig, axs = plt.subplots(1)
+    axs.plot(xss, res, label="h-hash, s-sort, w-write")
+    axs.set_title('Hash - thread performance')
+    axs.legend()
+    axs.set_ylabel('Elapsed time(s)')
+    plt.xticks(rotation=90)  # Direct rotation
 
     fig.tight_layout()
-    plt.savefig('net.png')
+    plt.savefig('overall.png')
 
 
 if __name__ == "__main__":
     # build_file_loc('cpu')
     extract_info()
-    # plot_info_data()
+    plot_data()
     # print(net_data)
